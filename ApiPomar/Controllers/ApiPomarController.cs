99909c67be08ce5e1d9393a9b25d99389b444d94
SENIOR\thiago.mazolli <thiago.mazolli@mega.com.br>
2021-01-29 01:11:11 -0300

    Criação da estrutura da api

---
+++ ApiPomar/Controllers/ApiPomarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ApiPomar.Controllers
+{
+	[EnableCors("*", "*", "*")]
+	[RoutePrefix("api/ApiPomar")]
+	public class ApiPomarController : ApiController
+	{
+		[AcceptVerbs("GET", "POST")]
+		[HttpPost]
+		[Route("Teste")]
+		public string Teste()
+		{
+			return "Teste API Pomar OK";
+		}
+
+		// GET: api/ApiPomar/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST: api/ApiPomar
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT: api/ApiPomar/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE: api/ApiPomar/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
